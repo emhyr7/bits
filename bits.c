3c5b13ea764bc08643fd520795b09b2202f8756d
Emhyr <kinglanchester@gmail.com>
2025-01-29 12:56:34 -0500

    .

--- bits.c
+++ bits.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <immintrin.h>
-#include <assert.h>
 #include <intrin.h>
 #include <stdlib.h>
 #include <memory.h>
@@ -25,27 +24,59 @@ typedef struct {
 
 static BitLocation FindClearBit2(Bits64 *p, Bits64 *q) {
 	BitLocation result;
-	Index i;
 	__mmask8 m;
 	__m512i v, cmp;
 
 	cmp = _mm512_set1_epi64(-1);
 	while (p < q) {
 		v = _mm512_load_epi64(p);
-		m = _mm512_cmplt_epi64_mask(v, cmp);
+		m = _mm512_cmplt_epu64_mask(v, cmp);
 		if (m) break;
 		p += 8;
 	}
-finish:
-	if (_BitScanForward64(&i, m)) {
-		p += i;
-		result.pointer = p;
-		(void)_BitScanForward64(&result.index, ~*p);
-		return result;
-	} else {
-		result = (BitLocation){0, 0};
-		return result;
+
+	int tzcnt = _tzcnt_u32(m);
+	result.pointer = p + tzcnt;
+	if (result.pointer < q) (void)_BitScanForward64(&result.index, ~*result.pointer);
+	return result;
+}
+
+BitLocation correct;
+
+/* don't use */
+static BitLocation FindClearBits2(int n, Bits64 *p, Bits64 *q) {
+	BitLocation result;
+	unsigned long i;
+	__mmask8 mask;
+	__m512i v, vl, vh, vmask, vcmp, vlzcnt, vomask;
+	
+	Assert(n <= 32);
+
+	vomask = _mm512_set1_epi64(-1);
+	vmask = _mm512_set1_epi64(n);
+	while (p < q) {
+		v = _mm512_load_epi64(p);
+		for (;;) {
+			mask = _mm512_cmpeq_epi64_mask(v, vomask);
+			if (mask == 0xff) break;
+			vlzcnt = _mm512_lzcnt_epi64(v);
+			mask = _mm512_cmpge_epi64_mask(vlzcnt, vmask);
+			if (mask) goto finish;
+			v = _mm512_sllv_epi64(v, vlzcnt);
+			v = _mm512_xor_epi64(v, v);
+			vlzcnt = _mm512_lzcnt_epi64(v);
+			v = _mm512_sllv_epi64(v, vlzcnt);
+			v = _mm512_xor_epi64(v, v);
+		}
+		mask = 0;
+
+		p += 8;
 	}
+finish:
+	int tzcnt = _tzcnt_u32(mask);
+	result.pointer = p + tzcnt;
+	result.index = 0;
+	return result;
 }
 
 size_t ClockRate;
@@ -65,20 +96,29 @@ int main(void) {
 	memset(p, -1, N);
 	unsigned long long random;
 	_rdrand64_step(&random);
-	memset(&p[random % M], random, sizeof(__m512i));
-	Bits64 *correct_p = &p[random % M];
-	BitLocation correct = {correct_p};
-	(void)_BitScanForward64(&correct.index, ~*correct_p);
+	correct.pointer = p + random % M;
+	memset(correct.pointer, random, sizeof(__m512i));
+	Assert(_BitScanForward64(&correct.index, ~*correct.pointer));
+	printf("random %% M: %llu\n", random % M);
+	printf("random: %llu\n", random);
+	printf("correct.pointer: %p -> %llu\n", correct.pointer, *correct.pointer);
+	printf("correct.index: %lu\n", correct.index);
 
-	BitLocation result[1];
+	BitLocation result[2];
 
 	QueryPerformanceCounter(&Start);
 	result[0] = FindClearBit2(p, q);
 	QueryPerformanceCounter(&End);
 	printf("Elapse: %llu\n", Elapse());
+	Assert(result[0].index == correct.index);
+	Assert(result[0].pointer == correct.pointer);
 
-	printf("%p & %ld\n", correct.pointer, correct.index);
+	QueryPerformanceCounter(&Start);
+	result[1] = FindClearBits2(1, p, q);
+	QueryPerformanceCounter(&End);
+	printf("Elapse: %llu\n", Elapse());
+
 	for (int i = 0; i < (sizeof(result) / sizeof(result[0])); ++i) {
-		printf("%p & %lu\n", result[i].pointer, result[i].index);
+		printf("[%i]: %p & %lu\n", i, result[i].pointer, result[i].index);
 	}
 }
