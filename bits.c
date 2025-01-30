5d21f57cb3c4a670b45ae197eccdf71cf105affa
Emhyr <kinglanchester@gmail.com>
2025-01-30 13:15:09 -0500

    .

--- bits.c
+++ bits.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <immintrin.h>
-#include <intrin.h>
 #include <stdlib.h>
 #include <memory.h>
+
+#if defined(_WIN32)
+
+#include <intrin.h>
 
 extern int __stdcall QueryPerformanceCounter  (unsigned long long *);
 extern int __stdcall QueryPerformanceFrequency(unsigned long long *);
 
 extern void *__stdcall VirtualAlloc(void *, unsigned long long, unsigned, unsigned);
 
-#define Assert(x) do { if (!(x)) __debugbreak(); } while (0)
+#endif
+
+#if defined(__clang__) || defined(__GNUC__)
+#define TRAP() __builtin_debugtrap()
+#elif defined(_MSC_VER)
+#define TRAP() __debugbreak()
+#endif
+
+#define Assert(x) do { if (!(x)) TRAP(); } while (0)
 
 typedef long long          Bits64;
 typedef unsigned long long Size;
@@ -25,12 +36,26 @@ typedef struct {
 
 #define LMASK(x) (~(-1ll << (x)))
 
-static inline BitLocation DoFindBit(Boolean reverse, Boolean clear, Bits64 *p, Bits64 *q) {
+typedef Word Flags;
+
+#if defined(__clang__) || defined(__GNUC__)
+#define _ffsll(x) __builtin_ffsll(x)
+#else
+inline int _ffsll(long long int x) {
+	int r;
+	if (!_BitScanForward64(x)) r = -1;
+	return r + 1;
+}
+#endif
+
+/* NOTE(Emhyr): "reverse" means to decrement the word pointer. it doesn't scan the bits reversely */
+
+static inline BitLocation DoFindBit(Boolean reverse, Boolean toClear, Bits64 *p, Bits64 *q) {
 	BitLocation result;
 	__mmask8 m;
 	__m512i v, cmp;
 	
-	cmp = _mm512_set1_epi64(clear ? -1 : 0);
+	cmp = _mm512_set1_epi64(toClear ? -1 : 0);
 	if (reverse) p -= 7;
 	for (;;) {
 		if (p == q) break;
@@ -50,22 +75,32 @@ static inline BitLocation DoFindBit(Boolean reverse, Boolean clear, Bits64 *p, B
 	result.pointer = p + zcnt;
 	if (result.pointer != q) {
 		Bits64 x = *result.pointer;
-		if (clear) x = ~x;
-		(void)_BitScanForward64(&result.index, x);
+		if (toClear) x = ~x;
+		result.index = _ffsll(x) - 1;
 	}
 	return result;
 }
 
-inline BitLocation FindSetBit         (Bits64 *p, Bits64 *q) { return DoFindBit(0, 0, p, q); }
-inline BitLocation FindClearBit       (Bits64 *p, Bits64 *q) { return DoFindBit(0, 1, p, q); }
-inline BitLocation ReverseFindSetBit  (Bits64 *p, Bits64 *q) { return DoFindBit(1, 0, p, q); }
-inline BitLocation ReverseFindClearBit(Bits64 *p, Bits64 *q) { return DoFindBit(1, 1, p, q); }
+BitLocation FindSetBit         (Bits64 *p, Bits64 *q) { return DoFindBit(0, 0, p, q); }
+BitLocation FindClearBit       (Bits64 *p, Bits64 *q) { return DoFindBit(0, 1, p, q); }
+BitLocation ReverseFindSetBit  (Bits64 *p, Bits64 *q) { return DoFindBit(1, 0, p, q); }
+BitLocation ReverseFindClearBit(Bits64 *p, Bits64 *q) { return DoFindBit(1, 1, p, q); }
 
 #define WIDTHOF(x) (sizeof(x) * 8)
-#define _ffsll(x)  __builtin_ffsll(x)
 
-BitLocation FindClearBits(Size n, Bits64 *p, Bits64 *q) {
-	if (n == 1) return FindClearBit(p, q);
+/* NOTE(Emhyr): if n <= half a word size, we can buffer-shift bits into a word to
+compare with a bitmask. */
+
+BitLocation DoFindBits(Boolean reverse, Boolean toClear, Size n, Bits64 *p, Bits64 *q) {
+	if (n == 1) {
+		if (reverse) {
+			if (toClear) return ReverseFindClearBit(p, q);
+			else return ReverseFindSetBit(p, q);
+		} else {
+			if (toClear) return FindClearBit(p, q);
+			else return FindSetBit(p, q);
+		}
+	}
 	
 	BitLocation result;
 	Size c, i, j, z;
@@ -74,8 +109,9 @@ BitLocation FindClearBits(Size n, Bits64 *p, Bits64 *q) {
 	result.pointer = p;
 	result.index = 0;
 	c = 0;
-	for (; p < q; ++p) {
-		w = ~*p;
+	for (; p != q; reverse ? --p : ++p) {
+		w = *p;
+		if (toClear) w = ~w;
 	retry:
 		result.pointer = p;
 		i = _ffsll(w);
@@ -91,7 +127,8 @@ BitLocation FindClearBits(Size n, Bits64 *p, Bits64 *q) {
 		if (j == WIDTHOF(*p) + 1) {
 			do {
 				w = *++p;
-				if (p >= q) goto finished;
+				if (!toClear) w = ~w;
+				if (p == q) goto finished;
 				z = _tzcnt_u64(w);
 				c += z;
 				if (c >= n) goto finished;
@@ -103,9 +140,14 @@ BitLocation FindClearBits(Size n, Bits64 *p, Bits64 *q) {
 		goto retry;
 	}
 finished:
-	if (p >= q) result = (BitLocation){0, 0};
+	if (p == q) result = (BitLocation){0, 0};
 	return result;
 }
+
+BitLocation FindSetBits         (Size n, Bits64 *p, Bits64 *q) { return DoFindBits(0, 0, n, p, q); }
+BitLocation FindClearBits       (Size n, Bits64 *p, Bits64 *q) { return DoFindBits(0, 1, n, p, q); }
+BitLocation ReverseFindSetBits  (Size n, Bits64 *p, Bits64 *q) { return DoFindBits(1, 0, n, p, q); }
+BitLocation ReverseFindClearBits(Size n, Bits64 *p, Bits64 *q) { return DoFindBits(1, 1, n, p, q); }
 
 BitLocation correct;
 
@@ -123,8 +165,8 @@ Bits64 *q = p + N;
 int main(int argc, char *argv[]) {
 	QueryPerformanceFrequency(&ClockRate);
 	memset(p, -1, N * sizeof(Bits64));
-	unsigned long long random = N - 1;
-	//_rdrand64_step(&random);
+	unsigned long long random = 128;
+	_rdrand64_step(&random);
 	correct.pointer = p + random % N;
 	memset(correct.pointer, random, sizeof(Bits64));
 	Assert(_BitScanForward64(&correct.index, ~*correct.pointer));
@@ -154,7 +196,14 @@ int main(int argc, char *argv[]) {
 	
 	printf("FindClearBits:\n");
 	QueryPerformanceCounter(&Start);
-	result = FindClearBits(3, p, q);
+	result = FindClearBits(1, p, q);
+	QueryPerformanceCounter(&End);
+	printf("\telapse: %llu\n", Elapse());
+	printf("\tresult: %p -> %llu & %lu\n", result.pointer, result.pointer ? *result.pointer : 0, result.index);
+	
+	printf("FindClearBits:\n");
+	QueryPerformanceCounter(&Start);
+	result = ReverseFindClearBits(1, q - 1, p - 1);
 	QueryPerformanceCounter(&End);
 	printf("\telapse: %llu\n", Elapse());
 	printf("\tresult: %p -> %llu & %lu\n", result.pointer, result.pointer ? *result.pointer : 0, result.index);
